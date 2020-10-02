074a276f47d171ff1fc1f4c7e6a94e35530fb63b
Deniel90 <daniel.rendek@gmail.com>
2020-10-02 18:52:22 +0200

    Add project files.

---
+++ Lotto/KorabbiSzamok.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class KorabbiSzamok
+    {
+        private int szam1, szam2, szam3, szam4, szam5;
+
+        public KorabbiSzamok(int szam1, int szam2, int szam3, int szam4, int szam5)
+        {
+            this.szam1 = szam1;
+            this.szam2 = szam2;
+            this.szam3 = szam3;
+            this.szam4 = szam4;
+            this.szam5 = szam5;
+        }
+
+        public int Szam1 { get => szam1; set => szam1 = value; }
+        public int Szam2 { get => szam2; set => szam2 = value; }
+        public int Szam3 { get => szam3; set => szam3 = value; }
+        public int Szam4 { get => szam4; set => szam4 = value; }
+    }
+}
---
+++ Lotto/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Készíts programot, amely bekér öt számot 1 és 90 tartományból. 
+            A számok nem ismétlődhetnek. A program írja ki, hogy az eddigi számhúzások alapján (minden héten megjátszotta) 
+            hány darab kettes, hármas, négyes, vagy ötös találata lett volna.
+            */
+
+            List<int> szamok = new List<int>();
+            Console.WriteLine("Lottósorsoláshoz kérek 5 különböző számot 1-90-ig");
+            int db = 0;
+            int szam;
+            do
+            {
+                do
+                {
+                    Console.Write((db + 1) + " szám: ");
+                } while (!Int32.TryParse(Console.ReadLine(), out szam) || (szam < 1 || szam > 90));
+
+                if (!szamok.Contains(szam))
+                {
+                    db++;
+                    szamok.Add(szam);
+                }
+
+            } while (szamok.Count < 5);
+
+
+            List<KorabbiSzamok> korabbiszamok = new List<KorabbiSzamok>();
+            StreamReader sr = new StreamReader("otos.csv");
+            while (!sr.EndOfStream)
+            {
+                string[] temp = sr.ReadLine().Split(';');
+                korabbiszamok.Add(new KorabbiSzamok(int.Parse(temp[11]), int.Parse(temp[12]), int.Parse(temp[13]), int.Parse(temp[14]), int.Parse(temp[15])));
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
